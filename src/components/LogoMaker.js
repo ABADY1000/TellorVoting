ddc194e5c4cff103101992686ea6f387afce96ab
Abdullah <abady1000@gmail.com>
2022-01-25 10:29:41 +0300

    Add LogoMaker module to seperate the process of creating the logo

--- src/components/LogoMaker.js
+++ src/components/LogoMaker.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import '../styles/Nav.css';
+import '../styles/LogoMaker.css';
 import { ReactComponent as TellorWhite } from '../assets/TellorWhite.svg';
 
 function LogoMaker(){
     return(
-        <TellorWhite className="NavBar__Title" />
+        <div>
+            <a href="https://tellor.io/" className="LogoMaker__link">
+                <div className='LogoMaker__container'>
+                    <div>
+                        <TellorWhite className="LogoMaker__Title"/>
+                    </div>
+                    <div className='LogoMaker__unstop'>
+                        <span>unstopable<br/>oracle</span>
+                    </div>
+                </div>
+            </a>
+        </div>
     );
 }
 
