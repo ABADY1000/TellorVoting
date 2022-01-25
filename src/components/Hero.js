b7bfbe2def4d2f278674ff768edcbbcc8d0fb62f
Abdullah <abady1000@gmail.com>
2022-01-25 10:32:18 +0300

    Arranging the page by modifying Hero and Nav components

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,6 +11,8 @@ import MetaMaskErrModal from "./MetaMaskErrModal";
 import TxnLoader from "./TxnLoader";
 import TxnModal from "./TxnModal";
 
+import {Table, Grid, Header, Button} from 'semantic-ui-react';
+
 function Hero({ currAddr, signer }) {
     //Component State
     const [loading, setLoading] = useState(false);
@@ -112,94 +114,140 @@ function Hero({ currAddr, signer }) {
     return (
         <div className="Hero">
             <div className="Hero__View">
-                <div className="flex-item">
-                    <h1> What are Tellor Treasuries?</h1>
-                    <h2>
-                        Stake your TRB and vote on the future monetary policy of Tellor, while
-                        earning rewards in the process. This new type of open market community
-                        will decide the circulating supply of TRB and future rates of return
-                        for staking TRB.
-                    </h2>
-                    <h2 className="Hero__LinkToWhitePaper bold">
-                        For more info read about it{" "}
-                        <a
-                            href="https://docs.tellor.io/tellor/whitepaper/tellor-oracle-overview/monetary-policy"
-                            alt="https://docs.tellor.io/tellor/whitepaper/tellor-oracle-overview/monetary-policy"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="bold special"
-                        >
-                            here
-                        </a>
-                        .
-                    </h2>
-                    <h2 className="Hero__LinkToWhitePaper bold">
-                        Watch the tech talk on it{" "}
-                        <a
-                            href="https://www.youtube.com/watch?v=25iawqdtmCs&t=682s"
-                            alt="https://www.youtube.com/watch?v=25iawqdtmCs&t=682s"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="bold special"
-                        >
-                            here
-                        </a>
-                        .
-                    </h2>
-                </div>
-                <div className="flex-item">
-                    <h1>What am I voting on exactly?</h1>
-                    <h2>
-                        This is the vote to kick off the first Treasury which determines the
-                        parameters such as: the max amount of TRB that can be locked up, the
-                        rate of return, and the duration of the treasury period.{" "}
-                    </h2>
-                    <h2>
-                        <span className="bold underline">
-                            For the first Treasury we are proposing:
-                        </span>
-                    </h2>
-                    <h2>
-                        Max amount of TRB: <span className="bold">100,000 TRB</span>
-                    </h2>
-                    <h2>
-                        Rate of return: <span className="bold">2.5% (approx. 10% APR)</span>
-                    </h2>
-                    <h2>
-                        Duration: <span className="bold">90 days</span>
-                    </h2>
-                </div>
+                <Grid stackable padded='horizontally'>
+                    <Grid.Row columns='equal' divided >
+                        <Grid.Column>
+                            <div className="Hero__decription-container">
+                                <h1> What are Tellor Treasuries?</h1>
+                                <h2>
+                                    Stake your TRB and vote on the future monetary policy of Tellor, while
+                                    earning rewards in the process. This new type of open market community
+                                    will decide the circulating supply of TRB and future rates of return
+                                    for staking TRB.
+                                </h2>
+                                <h2 className="Hero__LinkToWhitePaper bold">
+                                    For more info read about it{" "}
+                                    <a
+                                        href="https://docs.tellor.io/tellor/whitepaper/tellor-oracle-overview/monetary-policy"
+                                        alt="https://docs.tellor.io/tellor/whitepaper/tellor-oracle-overview/monetary-policy"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="bold special"
+                                        >
+                                        here
+                                    </a>
+                                    .
+                                </h2>
+                                <h2 className="Hero__LinkToWhitePaper bold">
+                                    Watch the tech talk on it{" "}
+                                    <a
+                                        href="https://www.youtube.com/watch?v=25iawqdtmCs&t=682s"
+                                        alt="https://www.youtube.com/watch?v=25iawqdtmCs&t=682s"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="bold special"
+                                        >
+                                        here
+                                    </a>
+                                    .
+                                </h2>
+                            </div>
+                        </Grid.Column>
+                        <Grid.Column >
+                            <div className="Hero__description_container">
+                                <h1>What am I voting on exactly?</h1>
+                                <h2>
+                                    This is the vote to kick off the first Treasury which determines the
+                                    parameters such as: the max amount of TRB that can be locked up, the
+                                    rate of return, and the duration of the treasury period.{" "}
+                                </h2>
+                                <div className='Hero__divider'></div>
+                                <h2>
+                                    <span className="bold underline">
+                                        For the first Treasury we are proposing:
+                                    </span>
+                                </h2>
+                                <div className="Hero__description_container_table">
+                                    <Table definition >
+                                        <Table.Body>
+                                            <Table.Row className="row">
+                                                <Table.Cell className="Hero__row_header"> Max TRB </Table.Cell>
+                                                <Table.Cell className="Hero__row_item"> 100,000 TRB </Table.Cell>
+                                            </Table.Row>
+                                            <Table.Row className="row">
+                                                <Table.Cell className="Hero__row_header"> Rate of return </Table.Cell>
+                                                <Table.Cell className="Hero__row_item"> 2.5% ( ~10% APR) </Table.Cell>
+                                            </Table.Row>
+                                            <Table.Row className="row">
+                                                <Table.Cell className="Hero__row_header"> Duration </Table.Cell>
+                                                <Table.Cell className="Hero__row_item"> 90 days </Table.Cell>
+                                            </Table.Row>
+                                        </Table.Body>
+                                    </Table>
+                                </div>
+                            </div>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>  
             </div>
-            <div>
-                <hr className="hr" />
-                <h2 className="startEndDate">
-                    -- Voting Opens: January 14th, 2022 @ 2pm EST --
-                </h2>
-                <h2 className="startEndDate">
-                    -- Voting Closes: January 21st, 2022 @ 2pm EST --
-                </h2>
-
-                <div className="Hero__CTAContainer">
-                    <div className="Hero__CTAColumn">
-                        <h2>Click here to vote in favor of this proposal</h2>
-                        <button
-                            onClick={() => handleVote(true)}
-                            className="Hero__VoteInFavor"
-                        >
-                            Vote in Favor
-                        </button>
-                    </div>
-                    <div className="Hero__CTAColumn">
-                        <h2>Click here to vote in opposition of this proposal</h2>
-                        <button
-                            onClick={() => handleVote(false)}
-                            className="Hero__VoteInOpposition"
-                        >
-                            Vote in Opposition
-                        </button>
-                    </div>
-                </div>
-            </div>
+            <hr className="Hero__line_breaker" />      
+            <div className="Hero__lower_half_container">
+                <Grid stackable>
+                    <Grid.Row textAlign="center" columns='equal'>
+                        {/* <Grid.Column width={3}></Grid.Column> */}
+                        <Grid.Column >
+                        
+                            <div className="Hero__duration_container">
+                                <h2 className="Hero__Duration">Duration</h2>
+                                <div className="Hero__duration_container_table">
+                                    <Table definition>
+                                        <Table.Row>
+                                            <Table.Cell>
+                                                <label className='Hero__row_header'>
+                                                    Open
+                                                </label>
+                                            </Table.Cell>
+                                            <Table.Cell className="Hero__date_font">January 14th, 2022 @ 2pm EST</Table.Cell>
+                                        </Table.Row>
+                                        <Table.Row>
+                                            <Table.Cell>
+                                                <label className='Hero__row_header'>
+                                                    Close
+                                                </label>
+                                            </Table.Cell>
+                                            <Table.Cell className="Hero__date_font">January 21st, 2022 @ 2pm EST</Table.Cell>
+                                        </Table.Row>
+                                    </Table>           
+                                </div>
+                            </div>
+                        </Grid.Column>
+                        {/* <Grid.Column width={3}></Grid.Column> */}
+                        <Grid.Column >
+                            <div className='Hero__buttons-container'>
+                                <h2 className='Hero__your_vote'>Your Vote</h2>
+                                <div>
+                                    <button
+                                        onClick={() => handleVote(true)}
+                                        className="Hero__VoteInFavor"
+                                        >
+                                        Vote in Favor
+                                    </button>
+                                </div>
+                                <br/>
+                                <div>
+                                    <button
+                                        onClick={() => handleVote(false)}
+                                        className="Hero__VoteInOpposition"
+                                        >
+                                        Vote in Opposition
+                                    </button>
+                                </div>
+                            </div>
+                        </Grid.Column> 
+                    </Grid.Row>
+                </Grid>
+           
+            </div>  
             <ErrModal innerRef={ref}>{[errMessage, setErrMessage]}</ErrModal>
             <TxnLoader loading={loading} />
             <TxnModal
@@ -214,3 +262,26 @@ function Hero({ currAddr, signer }) {
 }
 
 export default Hero;
+
+
+ {/* </div>    
+                <div className="Hero__CTAContainer">
+                <div className="Hero__CTAColumn">
+                <h2>Click here to vote in favor of this proposal</h2>
+                <button
+                onClick={() => handleVote(true)}
+                            className="Hero__VoteInFavor"
+                        >
+                            Vote in Favor
+                        </button>
+                    </div>
+                    <div className="Hero__CTAColumn">
+                        <h2>Click here to vote in opposition of this proposal</h2>
+                        <button
+                            onClick={() => handleVote(false)}
+                            className="Hero__VoteInOpposition"
+                        >
+                            Vote in Opposition
+                        </button>
+                    </div>
+                </div>*/}
